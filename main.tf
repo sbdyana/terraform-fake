d742193c19b7f319f37b47b08a69ad3814066785
sbdyana <101381931+sbdyana@users.noreply.github.com>
2025-12-09 15:18:38 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main_vpc" {
-  cidr_block           = "192.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
